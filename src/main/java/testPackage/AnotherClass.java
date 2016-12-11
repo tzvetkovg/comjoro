e13559446c816932a2d39c7be3742820ae49539c
Georgi <georgi.tzvetkov1988@gmail.com>
2016-12-11 22:56:37 +0000

    fff

--- src/main/java/testPackage/AnotherClass.java
+++ src/main/java/testPackage/AnotherClass.java
@@ -1,5 +1,7 @@
 package testPackage;
 
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 /**
  * Created by joreto on 11/12/2016.
  */
@@ -12,9 +14,13 @@ public class AnotherClass extends AbsractClass {
     public void setFs(String fs) {
         this.fs = fs;
     }
+    /*
+
+     */
     Integer af =2;
     String fs = "677667767622";
     Integer ass;
+    InternalResourceViewResolver a;
     public void callMe(String newParameter, Integer inter, String fssfs, String fs) { //Todo
 
     }
