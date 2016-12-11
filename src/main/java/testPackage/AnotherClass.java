b5957142a4a88306e05d1b3a9e4e70580a9782af
tzvetkovg <githubgt@gmail.com>
2016-12-11 20:46:52 +0000

    Update AnotherClass.java

--- src/main/java/testPackage/AnotherClass.java
+++ src/main/java/testPackage/AnotherClass.java
@@ -13,7 +13,7 @@ public class AnotherClass extends AbsractClass {
         this.fs = fs;
     }
 
-    String fs;
+    String fs = "123";
 
     public void callMe(String newParameter, Integer inter, String fssfs, String fs) { //Todo
 
