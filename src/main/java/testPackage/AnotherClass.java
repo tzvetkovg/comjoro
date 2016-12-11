ccb3f17be71dade0d7d9806aba5a9ee4bf6d8572
Georgi <georgi.tzvetkov1988@gmail.com>
2016-12-11 20:57:10 +0000

    conflictss

--- src/main/java/testPackage/AnotherClass.java
+++ src/main/java/testPackage/AnotherClass.java
@@ -13,7 +13,7 @@ public class AnotherClass extends AbsractClass {
         this.fs = fs;
     }
 
-    String fs = "6776677676";
+    String fs = "677667767622";
 
     public void callMe(String newParameter, Integer inter, String fssfs, String fs) { //Todo
 
